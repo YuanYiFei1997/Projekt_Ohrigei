18e95ba81eb9f4bb95ac3c9ef006b510bba28333
yifei.yuan <yifei.yuan@openjawtech.com>
2019-11-22 11:23:04 +0800

    firstcommit

---
+++ src/main/java/edu/dlufl/ohrigei/OhrigeiApplication.java
@@ -0,0 +1,13 @@
+package edu.dlufl.ohrigei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhrigeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhrigeiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/dlufl/ohrigei/OhrigeiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dlufl.ohrigei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhrigeiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
